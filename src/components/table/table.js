400dfcf0a76c062c6e1132e387bcc5fa4336421a
artur <123599698+gritchin-artur@users.noreply.github.com>
2024-03-25 17:17:25 +0100

    Update table.js

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -43,53 +43,56 @@ export function Table({ data }) {
           </th>
         </tr>
       </thead>
-      {data.results.map((word, item) => (
-        <tbody
-          className={data.results[0].progress >= 0 ? "WordOfTable" : ""}
-          key={item}
-          style={{ cursor: data.results[0].progress >= 0 ? "pointer" : "" }}
-        >
-          <tr
-            className="WordList"
-            onClick={(event) => {
-              word.progress >= 0 && handleClickWord(event, word);
-            }}
+      {data.results &&
+        data.results.map((word, item) => (
+          <tbody
+            className={data.results[0].progress >= 0 ? "WordOfTable" : ""}
+            key={item}
+            style={{ cursor: data.results[0].progress >= 0 ? "pointer" : "" }}
           >
-            <td className="TableHeaderItem">{word.en}</td>
-            <td className="TableHeaderItem">{word.ua}</td>
-            <td className="TableHeaderItem">{word.category}</td>
-            <td className="TableHeaderItem">
-              {word.progress >= 0 ? (
-                <div className="PercentContainer">
-                  {word.progress}%
-                  <Circle
-                    className="Circle"
-                    gapPosition="bottom"
-                    percent={word.progress}
-                    strokeWidth={15}
-                    trailWidth={15}
-                    strokeColor="#00FF00"
-                    strokeLinecap="round"
-                    gapDegree={0}
-                  />
-                </div>
-              ) : (
-                <div
-                  className="AddDictionaryContainer"
-                  onClick={() =>
-                    dispatch(addWord(word._id)).then((response) => {
-                      isNaN(response.payload) && navigate("/dictionary");
-                    })
-                  }
-                >
-                  <span className="AddToDictionaryText">Add to dictionary</span>
-                  <Switch />
-                </div>
-              )}
-            </td>
-          </tr>
-        </tbody>
-      ))}
+            <tr
+              className="WordList"
+              onClick={(event) => {
+                word.progress >= 0 && handleClickWord(event, word);
+              }}
+            >
+              <td className="TableHeaderItem">{word.en}</td>
+              <td className="TableHeaderItem">{word.ua}</td>
+              <td className="TableHeaderItem">{word.category}</td>
+              <td className="TableHeaderItem">
+                {word.progress >= 0 ? (
+                  <div className="PercentContainer">
+                    {word.progress}%
+                    <Circle
+                      className="Circle"
+                      gapPosition="bottom"
+                      percent={word.progress}
+                      strokeWidth={15}
+                      trailWidth={15}
+                      strokeColor="#00FF00"
+                      strokeLinecap="round"
+                      gapDegree={0}
+                    />
+                  </div>
+                ) : (
+                  <div
+                    className="AddDictionaryContainer"
+                    onClick={() =>
+                      dispatch(addWord(word._id)).then((response) => {
+                        isNaN(response.payload) && navigate("/dictionary");
+                      })
+                    }
+                  >
+                    <span className="AddToDictionaryText">
+                      Add to dictionary
+                    </span>
+                    <Switch />
+                  </div>
+                )}
+              </td>
+            </tr>
+          </tbody>
+        ))}
     </TableList>
   );
 }
